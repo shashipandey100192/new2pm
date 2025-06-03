8227a7a9244dd7a4c78a4cbc1e73e099a37662d7
shashi <shashiweb64@gmail.com>
2025-06-03 15:01:25 +0530

    create new project

---
+++ src/About.js
@@ -0,0 +1,53 @@
+import React,{Fragment} from "react"
+
+function Aboutpage()
+{
+    return <h1>heading one</h1>
+
+}
+export default Aboutpage
+
+
+export function About1()
+{
+    return <h2>this is name export function</h2>
+}
+
+// export function About2()
+// {
+//     return <section>
+//         <div>this is multi elements</div>
+//         <h3>heading three</h3>
+//         </section>
+// }
+
+// export function About2()
+// {
+//     return <>
+//         <div>this is multi elements</div>
+//         <h3>heading three</h3>
+//         </>
+// }
+
+
+
+export function About2()
+{
+    const a=100;
+
+    return <Fragment>
+        <div className="first">this is multi elements</div>
+        <h3 style={{color:'red',backgroundColor:"blue",fontSize:`${a}px`}}>heading three</h3>
+        </Fragment>
+}
+
+export function Mypage()
+{
+    return <div className="container border bg-danger">
+        <div className="row">
+            <div className="col-12">
+                <h1>use of bootstrap</h1>
+            </div>
+        </div>
+    </div>
+}
